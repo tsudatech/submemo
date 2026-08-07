bb92151760a81f0951879171e424495db77f4612
tsudatech <tsd.tch@gmail.com>
2026-08-07 22:31:00 +0900

    Initial Commit

---
+++ submemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  submemo
+//
+//  Created by 津田準 on 2026/08/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ submemo/submemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  submemoApp.swift
+//  submemo
+//
+//  Created by 津田準 on 2026/08/07.
+//
+
+import SwiftUI
+
+@main
+struct submemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
